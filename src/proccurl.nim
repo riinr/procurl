ae5978c5da687b7ac04986d48a7f310b8ef82af4
hugosenari <hugosenari@gmail.com>
2024-03-02 02:12:57 -0300

    feat: timing main

---
+++ src/proccurl.nim
@@ -0,0 +1,24 @@
+
+when isMainModule:
+  import std/[os]
+  proc main(): void =
+    if paramCount() >= 1 and 1.paramStr == "--protocols":
+      echo """
+{
+  "version": 0,
+  "transports": {
+    "mmap_boring": {
+      "speedRate": 10,
+      "options": {
+        "tailFormat": ["00x00", "02x32", "08x32", "16x16", "32x08"]
+        "headFormat": ["00x00", "02x32", "08x32", "16x16", "32x08"]
+      }
+    }
+  },
+  "formats": {
+    "flatty": { "speedRate": 10 }
+  }
+}"""
+      quit 0
+
+  main()
--- src/proccurl/rpc.nim
+++ src/proccurl/rpc.nim
@@ -1,4 +1,4 @@
-import std/[cmdline, os, memfiles]
+import std/[cmdline, memfiles]
 import boring
 
 const PAGE_SIZE = 4096
@@ -19,7 +19,7 @@ type
   Queue08x32* = Queue[08, MP[08], MC[08], array[32 * 1024, uint8]]
   Queue16x16* = Queue[16, MP[16], MC[16], array[16 * 1024, uint8]]
   Queue32x08* = Queue[32, MP[32], MC[32], array[08 * 1024, uint8]]
-  
+
   QueueObj* = object
     case format*: QueueFormat
     of qf00x00:
@@ -48,7 +48,7 @@ type
     tailQ*: ref QueueObj
 
 
-proc qSize(qFormat: QueueFormat): uint =
+proc qSize*(qFormat: QueueFormat): uint =
   case qFormat
     of qf02x32: static cast[uint](sizeOfQ[02, array[32 * 0001, uint8]]())
     of qf08x32: static cast[uint](sizeOfQ[08, array[32 * 1024, uint8]]())
@@ -57,13 +57,7 @@ proc qSize(qFormat: QueueFormat): uint =
     else:       static cast[uint](sizeOfQ[00, array[00 * 1024, uint8]]())
 
 
-proc qFormatFromParam(pos: int; isHead: bool): QueueFormat =
-  let format =
-    if paramCount() >= pos:
-      paramStr pos
-    else:
-      "00x00"
-
+proc qFormat*(format: string): QueueFormat =
   result = case format
     of "02x32": qf02x32
     of "08x32": qf08x32
@@ -71,46 +65,28 @@ proc qFormatFromParam(pos: int; isHead: bool): QueueFormat =
     of "32x08": qf32x08
     else:       qf00x00
 
-  let name =
-    if isHead:
-      "Head"
-    else:
-      "Tail"
-  let pos =
-    if isHead:
-      "first"
-    else:
-      "second"
 
-  doAssert result != qf00x00,
-    "Unknown " & name & "QueueFormat " &
-    $format &
-    "; inform " & name & "QueueFormat as " & pos & " parameter" &
-    "; know formats: 08x32, 16x16, 32x08" &
-    "; AxB means A Slots of B KB each"
-
-
-proc new00x00QueueObj*(arena: pointer): ref QueueObj =
+proc new00x00QueueObj(arena: pointer): ref QueueObj =
   result = new QueueObj
   result.format = qf00x00
   result.q00x00p = newQueue[00, MP[00], MC[00], array[00 * 1024, uint8]](arena)
 
-proc new02x32QueueObj*(arena: pointer): ref QueueObj =
+proc new02x32QueueObj(arena: pointer): ref QueueObj =
   result = new QueueObj
   result.format = qf02x32
   result.q02x32p = newQueue[02, MP[02], MC[02], array[32 * 0001, uint8]](arena)
 
-proc new08x32QueueObj*(arena: pointer): ref QueueObj =
+proc new08x32QueueObj(arena: pointer): ref QueueObj =
   result = new QueueObj
   result.format = qf08x32
   result.q08x32p = newQueue[08, MP[08], MC[08], array[32 * 1024, uint8]](arena)
 
-proc new16x16QueueObj*(arena: pointer): ref QueueObj =
+proc new16x16QueueObj(arena: pointer): ref QueueObj =
   result = new  QueueObj
   result.format =  qf16x16
   result.q16x16p = newQueue[16, MP[16], MC[16], array[16 * 1024, uint8]](arena)
 
-proc new32x08QueueObj*(arena: pointer): ref QueueObj =
+proc new32x08QueueObj(arena: pointer): ref QueueObj =
   result = new QueueObj
   result.format = qf32x08
   result.q32x08p = newQueue[32, MP[32], MC[32], array[08 * 1024, uint8]](arena)
@@ -124,11 +100,11 @@ proc newQueue*(mem: pointer; fmt: QueueFormat): ref QueueObj =
     else:       new00x00QueueObj mem
 
 
-proc headArena(ipcInfo: IpcInfo): pointer =
+proc headArena*(ipcInfo: IpcInfo): pointer =
   ipcInfo.memFile.mem
 
 
-proc tailArena(ipcInfo: IpcInfo): pointer =
+proc tailArena*(ipcInfo: IpcInfo): pointer =
   cast[pointer](
     cast[pointer](
       cast[uint](ipcInfo.memFile.mem) + MAX_SIZE
@@ -153,7 +129,6 @@ proc createIPCInfo*(headFormat, tailFormat: QueueFormat; memFileName: string; is
           mappedSize = (int(int(headSize + tailSize) / PAGE_SIZE) + 1) * PAGE_SIZE,
         )
 
-
   IpcInfo(
     headFormat:  headFormat,
     headSize:    headSize,
@@ -165,106 +140,130 @@ proc createIPCInfo*(headFormat, tailFormat: QueueFormat; memFileName: string; is
   )
 
 
-template createIPCInfo*(prefix = "/tmp/ipc-", suffix = "q.mmap"): IpcInfo =
-  let
-    headFormat  = 1.qFormatFromParam true
-    tailFormat  = 2.qFormatFromParam false
-    isMyFile    = paramCount() < 3
-    memFileName =
-      if isMyFile:
-        prefix & suffix
-      else:
-        3.paramStr
-
-  createIpcInfo(
-    headFormat,
-    tailFormat,
-    memFileName,
-    isMyFile,
-  )
-
-
-iterator qProducer*(q: ref QueueObj): uint8 {.closure.} =
-  var cur     = q.q02x32p.producer
-  var vRD     = RD
-  var i:uint8 = 2
-
-  while true:
-    var a = 0
-    while not q.q02x32p.enqueue(cur, i, vRD, sizeof(uint8).uint32):
-      yield i
-      vRD = RD
-      cur = q.q02x32p.producer
-      if a > 200:
-        break
-      else:
-        inc a
-
-    yield i
-    if i > 100:
-      break
+iterator producer*[T](q: ref QueueObj; v: var T): bool {.closure.} =
+  var vRD = RD
+  case q.format
+    of qf02x32:
+      var cur = q.q02x32p.producer
+      while true:
+        while not q.q02x32p.enqueue(cur, v, vRD, sizeof(uint8).uint32):
+          yield false
+          vRD = RD
+          cur = q.q02x32p.producer
+        yield true
     else:
-      inc i
+      yield false
 
 
-iterator qConsumer*(q: ref QueueObj): uint8 {.closure.} =
-  var cur     = q.q02x32p.consumer
-  var vWD     = WD
-  var i:uint8 = 0
+iterator consumer*[T](q: ref QueueObj; v: var T): bool {.closure.} =
+  var vWD = WD
+  case q.format
+    of qf02x32:
+      var cur = q.q02x32p.consumer
+      while true:
+        while not q.q02x32p.dequeue(cur, v, vWD):
+          yield false
+          vWD = WD
+          cur = q.q02x32p.consumer
+        yield true
+    else:
+      yield false
 
-  while true:
-    var a = 0
-    while not q.q02x32p.dequeue(cur, i, vWD):
-      yield i
-      vWD = WD
-      cur = q.q02x32p.consumer
-      if a > 200:
-        break
-      else:
-        inc a
-
-    yield i
-    if i > 95:
-      break
-
-
-proc main() =
-  var info = createIpcInfo(suffix = $getCurrentProcessId() & "-main.mmap")
-  defer:
-    close info.memFile
-    if not info.isMyFile:
-      removeFile info.memFileName
-
-  let ipc  = Ipc(
-    info:  info,
-    headQ: info.headArena.newQueue info.headFormat,
-    tailQ: info.tailArena.newQueue info.tailFormat,
-  )
-
-  echo "[main]"
-  echo "FileName = ",   '"', ipc.info.memFileName, '"'
-  echo "HeadFormat = ", '"', ipc.info.headFormat,  '"'
-  echo "TailFormat = ", '"', ipc.info.tailFormat,  '"'
-  echo "HeadSize = ",        ipc.info.headSize
-  echo "TailSize = ",        ipc.info.tailSize
-  echo "IsMyFile = ",        ipc.info.isMyFile
-
-  var p = qProducer
-  var c = qConsumer
-  if info.isMyFile:
-    while not (finished(p) and finished(c)):
-      sleep 100
-      if not finished(p):
-        echo 'p', p(ipc.headQ)
-      if not finished(c):
-        discard c(ipc.tailQ)
-  else:
-    while not (finished(p) and finished(c)):
-      sleep 100
-      if not finished(c):
-        echo 'c', c(ipc.headQ)
-      if not finished(p):
-        discard p(ipc.tailQ)
 
 when isMainModule:
+  import std/os
+  import std/[monotimes, times]
+
+  proc qFormatFromParam(pos: int; isHead: bool): QueueFormat =
+    let format =
+      if paramCount() >= pos:
+        paramStr pos
+      else:
+        "00x00"
+  
+    result = format.qFormat
+  
+    let name =
+      if isHead:
+        "Head"
+      else:
+        "Tail"
+    let pos =
+      if isHead:
+        "first"
+      else:
+        "second"
+
+    doAssert result != qf00x00,
+      "Unknown " & name & "QueueFormat " &
+      $format &
+      "; inform " & name & "QueueFormat as " & pos & " parameter" &
+      "; know formats: 08x32, 16x16, 32x08" &
+      "; AxB means A Slots of B KB each"
+
+  template createIPCInfo*(prefix = "/tmp/ipc-", suffix = "q.mmap"): IpcInfo =
+    let
+      headFormat  = 1.qFormatFromParam true
+      tailFormat  = 2.qFormatFromParam false
+      isMyFile    = paramCount() < 3
+      memFileName =
+        if isMyFile:
+          prefix & suffix
+        else:
+          3.paramStr
+  
+    createIpcInfo(
+      headFormat,
+      tailFormat,
+      memFileName,
+      isMyFile,
+    )
+
+  proc main() =
+    var info = createIpcInfo(suffix = $getCurrentProcessId() & "-main.mmap")
+    defer:
+      close info.memFile
+      if not info.isMyFile:
+        removeFile info.memFileName
+  
+    let ipc  = Ipc(
+      info:  info,
+      headQ: info.headArena.newQueue info.headFormat,
+      tailQ: info.tailArena.newQueue info.tailFormat,
+    )
+  
+    echo "{", '"', "main", '"', ": {",
+        '"', "FileName",    '"', ':', '"', ipc.info.memFileName, '"', ',',
+        '"', "HeadFormat",  '"', ':', '"', ipc.info.headFormat,  '"', ',',
+        '"', "TailFormat",  '"', ':', '"', ipc.info.tailFormat,  '"', ',',
+        '"', "HeadSize",    '"', ':', ' ', ipc.info.headSize,    ' ', ',',
+        '"', "TailSize",    '"', ':', ' ', ipc.info.tailSize,    ' ', ',',
+        '"', "IsMyFile",    '"', ':', '"', ipc.info.isMyFile,    '"', ' ',
+      "}}"
+  
+    var p = producer[uint8]
+    var c = consumer[uint8]
+    var s = getMonoTime()
+    var r = getMonoTime()
+    var i: uint8 = 0
+    if info.isMyFile:
+      while not (finished(p) and finished(c)):
+        if not finished(p) and p(ipc.headQ, i):
+          echo 's', inNanoseconds(getMonoTime() - s)
+          inc i
+          s = getMonoTime()
+        if not finished(c) and c(ipc.tailQ, i):
+          echo 'r', inNanoseconds(getMonoTime() - r)
+          r = getMonoTime()
+    else:
+      while not (finished(p) and finished(c)):
+        if not finished(c) and c(ipc.headQ, i):
+          discard
+          echo 'r', inNanoseconds(getMonoTime() - r)
+          r = getMonoTime()
+        if not finished(p) and p(ipc.tailQ, i):
+          echo 's', inNanoseconds(getMonoTime() - s)
+          inc i
+          s = getMonoTime()
+
   main()
